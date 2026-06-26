96b05728b310c2d6b8a7d85d5224f192a65bf554
Romeuads <romeu.anjos@aluno.fmpsc.edu.br>
2026-06-25 22:15:53 -0300

    Add configuration for Supabase URL and key

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  SUPABASE_URL: 'https://cjuthytglnwqkgewejpz.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdXRoeXRnbG53cWtnZXdlanB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMjc2MTUsImV4cCI6MjA5NjcwMzYxNX0.NLvkPu4EmreH8qiSJnc8NfxU5R1RzV4GGygk_rV_NaU',
+}
